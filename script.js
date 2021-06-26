983fa08d4ec33ad37373bfc9be3b9fec37ab0084
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-06-26 08:27:27 -0400

    build simple todo app

---
+++ script.js
@@ -0,0 +1,41 @@
+const form = document.querySelector("form");
+const submit = document.querySelector("button");
+const input = document.querySelector("input");
+let tasks=[];
+const taskList = document.getElementById("listTasks");
+
+
+form.addEventListener("submit", (e) => {
+  if(input.value === "" || input.value === null) {
+    e.preventDefault();
+    console.log("You haven't entered a task.");
+  } else {
+    e.preventDefault();
+    console.log("task entered successfully.");
+    localStorage.setItem('task', input.value);
+    let currentTask = localStorage.getItem('task');
+    if(currentTask !== "" && currentTask !== null) {
+      tasks.push(currentTask);
+      input.value="";
+    }
+    console.log(tasks);
+  }
+});
+
+let orderedTaskList = document.getElementById("orderedTaskList");
+
+listTasks.addEventListener("click", function() {
+  for(let i = 0; i < tasks.length; i++) {
+    const taskItem = document.createElement("li");
+    taskItem.innerText = tasks[i];
+    orderedTaskList.appendChild(taskItem);
+    taskItem.addEventListener("click", function() {
+      orderedTaskList.removeChild(taskItem);
+      console.log("click");
+    });
+  }
+});
+
+function permanentlyRemove() {
+  location.reload();
+}
